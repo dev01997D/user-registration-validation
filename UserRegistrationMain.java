14b4eedb7a97b43ff1dda23f1cd28a2974c02a9f
dev01997D <devnandan01997@gmail.com>
2020-09-30 23:18:13 +0530

    Added: Welcome message added

---
+++ UserRegistrationMain.java
@@ -0,0 +1,11 @@
+package com.bz.userregistration;
+
+public class UserRegistrationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to User Registration Validation Problem");
+	}
+
+}
